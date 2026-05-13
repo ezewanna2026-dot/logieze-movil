5d4bc16398ebbacbd1dc257c440e1d3b3fc98d0c
ezewanna2026-dot <ezewanna2026@gmail.com>
2026-05-13 19:44:17 -0300

    fix: copiar patches de la receta built-in al directorio local (p4a busca patches en recipe_dir local)

--- app_movil/p4a_recipes/hostpython3/__init__.py
+++ app_movil/p4a_recipes/hostpython3/__init__.py
@@ -1,4 +1,22 @@
+import os
+import shutil
+from os.path import dirname, join, exists, isdir
+
+import pythonforandroid.recipes.hostpython3 as _base_module
 from pythonforandroid.recipes.hostpython3 import HostPython3Recipe as _HostPython3Recipe
+
+# p4a sets recipe_dir to THIS local directory, so patches must exist here.
+# Copy everything from the built-in recipe directory so patches can be found.
+_base_dir = dirname(_base_module.__file__)
+_local_dir = dirname(__file__)
+for _item in os.listdir(_base_dir):
+    _src = join(_base_dir, _item)
+    _dst = join(_local_dir, _item)
+    if not exists(_dst):
+        if isdir(_src):
+            shutil.copytree(_src, _dst)
+        else:
+            shutil.copy2(_src, _dst)
 
 
 class HostPython3Recipe(_HostPython3Recipe):
